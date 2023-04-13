038aa50e1199318fd09a8513604cad4c7805b814
GBC034 <gbc034@outlook.com>
2023-04-13 18:27:42 +0800

    Authorize 1.1

--- src/mslogin.ts
+++ src/mslogin.ts
@@ -13,7 +13,7 @@ ipcMain.handle("mslogin", () =>
         "client_id=" + "391fbcc2-29ef-4c2f-82e1-2ed757b47f3c" +
         "&response_type=code" +
         "&prompt=login"
-        "&redirect_uri=https%3A%2F%2Flogin.microsoftonline.com%2Fconsumers%2Foauth2%2Fv2.0%2Fdesktop?" +
+        "&redirect_uri=https%3A%2F%2Flogin.live.com%2Foauth20_desktop.srf" +
         "&scope=XboxLive.signin"
 
       win.loadURL("https://login.live.com/oauth20_authorize.srf?client_id=00000000402b5328&response_type=code&prompt=login&scope=service%3A%3Auser.auth.xboxlive.com%3A%3AMBI_SSL&redirect_uri=https%3A%2F%2Flogin.live.com%2Foauth20_desktop.srf");
