78ad450a5b666d0959ea9f8ef4c421bb21ca2898
GBC034 <gbc034@outlook.com>
2023-04-13 18:25:50 +0800

    Authorize 1.0

--- src/mslogin.ts
+++ src/mslogin.ts
@@ -8,6 +8,14 @@ ipcMain.handle("mslogin", () =>
         autoHideMenuBar: true,
         frame: false
       });
+      let LoginURL =
+        "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?" +
+        "client_id=" + "391fbcc2-29ef-4c2f-82e1-2ed757b47f3c" +
+        "&response_type=code" +
+        "&prompt=login"
+        "&redirect_uri=https%3A%2F%2Flogin.microsoftonline.com%2Fconsumers%2Foauth2%2Fv2.0%2Fdesktop?" +
+        "&scope=XboxLive.signin"
+
       win.loadURL("https://login.live.com/oauth20_authorize.srf?client_id=00000000402b5328&response_type=code&prompt=login&scope=service%3A%3Auser.auth.xboxlive.com%3A%3AMBI_SSL&redirect_uri=https%3A%2F%2Flogin.live.com%2Foauth20_desktop.srf");
       win.webContents.on("will-redirect", (_ev, url) => {
         const prefix = "https://login.live.com/oauth20_desktop.srf?";
