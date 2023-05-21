15a30d4c12d7f94798b1b6fd81826aa6ecd62fa0
GBC034 <gbc034@outlook.com>
2023-05-21 13:05:20 +0800

    temp 3

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,17 +2,11 @@ import React from 'react';
 import { MCAuthMS } from 'auth/MinecraftAuth';
 import { MainPage } from './pages/Main';
 
-function pageSwitch(page: number): () => JSX.Element | JSX.Element {
-  switch(page) {
-    case 1:
-      return MainPage
-  }
-  return (<p>SB</p>)
-}
+
 
 
 export function App(): JSX.Element {
   return (
-      pageSwitch()
+      <MainPage></MainPage>
   );
 };
