d20a1d5bde87e461f9315765a9001afd181e66ae
Fiorest009 <fiorest_09@outlook.com>
2023-05-21 17:44:09 +0800

    buggggggggged

--- src/renderer/pages/Entry.tsx
+++ src/renderer/pages/Entry.tsx
@@ -2,7 +2,9 @@ import React from "react";
 export default function Entry() {
     return (
         <>
-            
+            <h1>
+                shit
+            </h1>
         </>
     )
 }
--- src/renderer/pages/Main.tsx
+++ src/renderer/pages/Main.tsx
@@ -1,8 +1,12 @@
 import { MCAuthMS } from "auth/MinecraftAuth";
+import Entry from "./Entry";
 import React from "react";
 import "./css/Entry.css"
 export function MainPage() {
     return (
+        <>
+        <Entry/>
         <button className="w-48" onClick={MCAuthMS}> MCAuth </button>
+        </>
     )
 }
