1e90b4bfc1fba4be0410a49aff8741d2eb7f472e
GBC034 <gbc034@outlook.com>
2023-06-22 15:30:34 +0800

    Electron-Forge

--- src/renderer/pages/Main.tsx
+++ src/renderer/pages/Main.tsx
@@ -7,7 +7,7 @@ import { ipcRenderer } from "electron";
 import { AuthenticateMC } from "src/core/Authenticate";
 
 async function downloader() {
-    ipcRenderer.invoke("downloadfile", "https://www.electronjs.org/assets/marketing/auto-updater.png", "C:/Users/GBC03/Desktop", "test", 10)
+    ipcRenderer.invoke("downloadfile", "https://github.com/electron/fiddle/releases/download/v0.32.9/electron-fiddle-0.32.9-win32-x64-setup.exe", "C:/Users/GBC03/Desktop", "test", 10)
 }
 export function MainPage() {
     return (
